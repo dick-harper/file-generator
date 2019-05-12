e823f847e4eee29f9bf732c9c416c837dd47d3e8
Dick Harper <dick.harper@gmail.com>
2019-05-12 12:04:35 +0000

    initial commit

---
+++ FileGeneratorLib/FileGenerator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace FileGeneratorLib
+{
+    public class FileGenerator
+    {
+        string _rootDirectory;
+        public FileGenerator(string rootDirectory)
+        {
+            _rootDirectory = rootDirectory;
+        }
+    }
+}
