f13ead57644f53455339a2ac2583cf0bf1f15e69
Dick Harper <dick.harper@gmail.com>
2019-05-12 12:27:38 +0000

    Add constructor

--- FileGeneratorLib/FileGenerator.cs
+++ FileGeneratorLib/FileGenerator.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace FileGeneratorLib
 {
@@ -7,6 +8,14 @@ namespace FileGeneratorLib
         string _rootDirectory;
         public FileGenerator(string rootDirectory)
         {
+            if(string.IsNullOrWhiteSpace(rootDirectory)){
+                throw new Exception("invalid root directory");
+            }
+
+            if(Directory.Exists(_rootDirectory)){
+                throw new DirectoryNotFoundException(_rootDirectory);
+            }
+
             _rootDirectory = rootDirectory;
         }
     }
